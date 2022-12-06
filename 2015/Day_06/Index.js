25f635f50d57ba70978ee8a9c8b7b4dbbae7b761
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-12-06 18:10:21 +0200

    Attempt to 2015-6

--- 2015/Day_06/Index.js
+++ 2015/Day_06/Index.js
@@ -1,17 +1,83 @@
 const { Input } = require('./Input');
 
-const lines = Input.trim().split('\n');
-
-function part1() {
-
+function read(inp) {
+  let instructions = [];
+  let values = [];
+  inp.trim().split('\n')
+    .forEach((line, i) => {
+      const parts = line.split(' ')
+      const instruction = parts.length == 5 ? parts[1] : parts[0];
+      instructions.push(instruction);
+      let obj, from, to;
+      if (parts.length === 5) {
+        let [xinit, yinit, x, y] = (parts[2] + ',' + parts[4]).split(',').map(p => Number(p));
+        from = { x: xinit, y: yinit };
+        to = { x, y };
+      } else {
+        let [xinit, yinit, x, y] = (parts[1] + ',' + parts[3]).split(',').map(p => Number(p));
+        from = { x: xinit, y: yinit };
+        to = { x, y };
+      }
+      obj = [from, to];
+      values.push(obj);
+    });
+  return [instructions, values];
 }
 
-function part2() {
+const grid = [];
+const data = read(Input);
 
+function initGrid() {
+  let rows = 1000;
+  let cols = 1000;
+  for (let x = 0; x < rows; x++) {
+    grid[x] = Array(cols).fill(false);
+  }
 }
 
-part1();
-part2();
+function part1(inp) {
+  initGrid();
+  const [instructions, values] = inp;
+
+  for (let i = 0; i < instructions.length; i++) {
+    const step = values[i];
+    let a = step[0].x
+    let b = step[0].y
+    let c = step[1].x
+    let d = step[1].y
+    const action = instructions[i];
+    lights(action, a, b, c, d);
+  }
+
+  let turnedOn = grid.flat().filter(val => val === true).length
+  return console.log(turnedOn);
+}
+
+
+function lights(action, a, b, c, d) {
+  let rowsStart = a;
+  let rowsEnd = c;
+  let colsStart = b;
+  let colsEnd = d;
+
+  for (let x = rowsStart; x <= rowsEnd; x++) {
+    for (let y = colsStart; y <= colsEnd; y++) {
+      switch (action) {
+        case 'on': grid[x][y] = true;
+        case 'off': grid[x][y] = false;
+        case 'toggle': grid[x][y] = !grid[x][y];
+      }
+    }
+  }
+}
+
+
+
+function part2(inp) {
+
+}
+part1(data);
+part2(data);
 
 //if one func
 // solution(Test, 1);
--- readme.md
+++ readme.md
@@ -10,5 +10,5 @@
 ``` npm run 2022-1 ```
 
 
-Currently only 2022 is present.
-Might do earlier ones sometime.  ¯\\_(ツ)_/¯
+Currently only 2022 and 2015 are in progress.
+Might do more sometime.  ¯\\_(ツ)_/¯
