fbf600fedfdcf779d8636f26319db10a66f3323b
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-12-08 21:26:30 +0200

    2022-8

--- 2022/Day_08/Index.js
+++ 2022/Day_08/Index.js
@@ -1,19 +1,66 @@
 const { Input, Test } = require('./Input');
 
-const lines = Input.trim().split('\n');
+const grid = Input
+  .split('\n').map((line) => line.split('').map(Number))
+  .map((row, y, grid) =>
+    row.map((tree, x) => {
+      const edge = x === 0 || y === 0 || x === row.length - 1 || y === grid.length - 1;
+      const value = tree;
+      return { edge, value };
+    })
+  );
 
-function part1() {
+function getEdges(x, y) {
+  const top = grid.slice(0, y).map((row) => row[x].value).reverse();
+  const right = grid[y].slice(x + 1, grid[y].length).map((value) => value.value);
+  const bottom = grid.slice(y + 1, grid.length).map((row) => row[x].value);
+  const left = grid[y].slice(0, x).map((value) => value.value).reverse();
 
+  return { top, right, bottom, left };
 }
 
-function part2() {
+function countVisible() {
+  return grid.reduce((visible, row, y) => {
+    row.forEach((tree, x) => {
+      if (tree.edge) { visible += 1; return; }
 
+      const edges = getEdges(x, y);
+      for (let direction of ['top', 'right', 'bottom', 'left']) {
+        const edgeTrees = edges[direction];
+
+        if (edgeTrees.every((edge) => tree.value > edge)) {
+          visible += 1;
+          break;
+        }
+      }
+    });
+    return visible;
+  }, 0);
 }
 
-part1();
-part2();
+function getScenicView() {
+  return grid.reduce((memo, row, y) => {
+    const scores = row.map((tree, x) => {
+      if (tree.edge) return 0;
+      const edges = getEdges(x, y);
+      const trees = [];
 
-//if one func
-// solution(Test, 1);
-// solution(Input, 1);
-// solution(Input, 2);
+      for (let direction of ['top', 'right', 'bottom', 'left']) {
+        const edgeTrees = (() => {
+          let count = 0;
+          for (const edgeTree of edges[direction]) {
+            count++;
+            if (edgeTree >= tree.value) break;
+          }
+          return count;
+        })();
+        trees.push(edgeTrees);
+      }
+      return trees.reduce((a, b) => a * b, 1);
+    });
+    return Math.max(...scores, memo);
+  }, 0);
+}
+
+console.log(countVisible()); //part 1: 1736
+console.log(getScenicView()); //part 2: 268800
