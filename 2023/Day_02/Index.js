a24a3f51747aeee5a7a1ddb04e3defb0f88e74c9
eyyMinda <gamerx14x@gmail.com>
2023-11-04 20:54:07 +0200

    pre-2023

---
+++ 2023/Day_02/Index.js
@@ -0,0 +1,19 @@
+const { Input, Test } = require('./Input');
+
+const lines = Input.trim().split('\n');
+
+function part1() {
+
+}
+
+function part2() {
+
+}
+
+part1();
+part2();
+
+//if one func
+// solution(Test, 1);
+// solution(Input, 1);
+// solution(Input, 2);
