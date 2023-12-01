129d59b33048fb5aeb6ac62fbabd9003454e5cd6
eyyMinda <gamerx14x@gmail.com>
2023-12-01 13:47:14 +0200

    2023/01

--- 2023/Day_01/Index.js
+++ 2023/Day_01/Index.js
@@ -1,19 +1,64 @@
-const { Input, Test } = require('./Input');
+const { Input, Test, Test2 } = require('./Input');
 
 const lines = Input.trim().split('\n');
 
-function part1() {
+const buildNumber = (digits) => {
+  if (digits.length === 0) {
+    return 0;
+  }
+  if (digits.length === 1) {
+    return parseInt(digits[0].toString() + digits[0].toString(), 10);
+  } else {
+    return parseInt(digits[0].toString() + digits[digits.length - 1].toString(), 10);
+  }
+};
 
+const convertStringsToNumbers = (line) => {
+  const wordToDigit = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+    zero: '0',
+  };
+
+  let newLine = '';
+  let wordBuffer = '';
+
+  for (let j = 0; j < line.length; j++) {
+    const currentChar = line[j].toLowerCase();
+
+    if (currentChar >= 'a' && currentChar <= 'z') {
+      // Accumulate characters to form a word
+      wordBuffer += currentChar;
+    } else {
+      // Check if we have a complete word in the buffer
+      if (wordToDigit[wordBuffer]) {
+        newLine += wordToDigit[wordBuffer];
+        wordBuffer = ''; // Reset the word buffer
+      } else if (isFinite(parseInt(currentChar, 10))) {
+        newLine += currentChar;
+      }
+    }
+  }
+
+  return newLine;
+};
+
+async function main() {
+  const numbers = lines.map(line => {
+    const convertedLine = convertStringsToNumbers(line);
+    const filtered = Array.from(convertedLine).map(c => parseInt(c, 10)).filter(n => !isNaN(n));
+    return buildNumber(filtered);
+  });
+
+  const result = numbers.reduce((a, b) => a + b);
+  console.log(result);
 }
 
-function part2() {
-
-}
-
-part1();
-part2();
-
-//if one func
-// solution(Test, 1);
-// solution(Input, 1);
-// solution(Input, 2);
+main();
