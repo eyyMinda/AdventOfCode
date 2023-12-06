8c9d3fb3dbf3276414e07f2696be04d003cdf7f4
eyyMinda <gamerx14x@gmail.com>
2023-12-06 15:49:45 +0200

    2023-6

--- 2023/Day_06/Index.js
+++ 2023/Day_06/Index.js
@@ -2,18 +2,31 @@ const { Input, Test } = require('./Input');
 
 const lines = Input.trim().split('\n');
 
-function part1() {
+const read = (str) => str.split(' ').filter(Boolean).filter(Number).map(n => +n);
+const read2 = (str) => [+(str.split(' ').filter(Boolean).filter(Number).join(''))];
 
+
+function solution(part) {
+  let res = 1;
+
+  const readFunc = part === 1 ? read : read2;
+  const [time, distance] = [readFunc(lines[0]), readFunc(lines[1])];
+  const arr = time.map((time, i) => ({ currTime: time, targetDist: distance[i] }));
+
+
+  for (let i = 0; i < arr.length; i++) {
+    let raceBeat = 0;
+    const { currTime, targetDist } = arr[i];
+    for (let ms = 1; ms < currTime; ms++) {
+      raceBeat += ms * (currTime - ms) > targetDist ? 1 : 0;
+    }
+
+    res *= raceBeat;
+  }
+
+  return res;
 }
 
-function part2() {
 
-}
-
-part1();
-part2();
-
-//if one func
-// solution(Test, 1);
-// solution(Input, 1);
-// solution(Input, 2);
+console.log(solution(1));
+console.log(solution(2));
--- 2023/Day_06/Input.js
+++ 2023/Day_06/Input.js
@@ -1,4 +1,6 @@
-const Test = ``;
-const Input = ``;
+const Test = `Time:      7  15   30
+Distance:  9  40  200`;
+const Input = `Time:        45     97     72     95
+Distance:   305   1062   1110   1695`;
 
 module.exports = { Input, Test };
