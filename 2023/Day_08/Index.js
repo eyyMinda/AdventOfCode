8abb4e7cfd05f77c94286df47bd530d6443f28b1
eyyMinda <gamerx14x@gmail.com>
2023-12-08 15:52:02 +0200

    2023-8

--- 2023/Day_08/Index.js
+++ 2023/Day_08/Index.js
@@ -1,19 +1,47 @@
-const { Input, Test } = require('./Input');
+const { Input, Test, Test2, Test3 } = require('./Input');
 
-const lines = Input.trim().split('\n');
+const RE = /(.*) = \((.*), (.*)\)/;
+const parse = input => {
+  const [directions, nodes] = input.split("\n\n");
+  return [
+    [...directions].map(v => (v === "L" ? 0 : 1)),
+    new Map(
+      nodes
+        .split("\n")
+        .map(row => RE.exec(row))
+        .map(([, from, l, r]) => [from, [l, r]])
+    ),
+  ];
+};
 
-function part1() {
 
-}
+const walk = ([dir, nodes]) => {
+  let curr = "AAA";
+  let steps = 0;
+  while (curr !== "ZZZ")
+    curr = nodes.get(curr)[dir[steps++ % dir.length]];
+  return steps;
+};
 
-function part2() {
+const walkNodes = ([dir, nodes]) =>
+  [...nodes]
+    .filter(([n]) => n.endsWith("A"))
+    .map(([curr]) => {
+      let steps = 0;
+      while (!curr.endsWith("Z"))
+        curr = nodes.get(curr)[dir[steps++ % dir.length]];
+      return steps;
+    });
 
-}
+const gcd = (a, b) => a ? gcd(b % a, a) : b;
+const lcm = (a, b) => a * b / gcd(a, b);
+const llcm = arr => arr.reduce((acc, v) => lcm(acc, v));
 
-part1();
-part2();
+const input = parse(Input);
 
-//if one func
-// solution(Test, 1);
-// solution(Input, 1);
-// solution(Input, 2);
+// Part 1
+const p1 = walk(input);
+console.log(p1)
+// Part 2
+const p2 = walkNodes(input);
+console.log(llcm(p2))
